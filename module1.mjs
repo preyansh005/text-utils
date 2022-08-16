b9d9371f791100a3cf3f16cd8449805a2cf95a3d
Patel Preyansh <patelpreyansh3011@gmail.com>
2022-08-16 14:01:31 +0530

    Video 6

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+const a = "Preyansh"
+const b = "Shivani"
+const c = "Hasumati"
+const d = "Sanjay"
+
+export default d
+export {a}
+export {b}
+export {c}
---
+++ module2.mjs
@@ -0,0 +1,3 @@
+import x , {a,b,c} from './module1.mjs'
+
+console.log(x,a,b,c)
